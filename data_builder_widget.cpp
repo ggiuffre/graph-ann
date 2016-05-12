877d945abdb7d659f7b72479b931109b34e08e8e
GIUFFRE' Giorgio [IN3] <ggiuffre@t45.torre.math.unipd.it>
2016-05-12 12:38:13 +0200

    data_builder err handling

--- data_builder_widget.cpp
+++ data_builder_widget.cpp
@@ -1,5 +1,6 @@
 #include "data_builder_widget.h"
 #include <QInputDialog>
+#include <QMessageBox>
 #include <fstream>
 
 dataBuilderWidget::dataBuilderWidget(QWidget * parent) : QWidget(parent), layout(new QFormLayout), input_buffer(new QTextEdit), target_buffer(new QTextEdit), next(new QPushButton("Aggiungi")), trigger(new QPushButton("Aggiungi e crea")), n_examples(0)
@@ -19,23 +20,39 @@ dataBuilderWidget::dataBuilderWidget(QWidget * parent) : QWidget(parent), layout
 
 void dataBuilderWidget::addExample()
 {
-	unsigned int in_count = input_buffer->toPlainText().split(" ").filter(QRegularExpression("\\d")).size();
-	unsigned int out_count = input_buffer->toPlainText().split(" ").filter(QRegularExpression("\\d")).size();
+	unsigned int in_count = input_buffer->toPlainText().split(" ").filter(QRegularExpression("\\d+")).size();
+	unsigned int out_count = target_buffer->toPlainText().split(" ").filter(QRegularExpression("\\d+")).size();
 	if (n_examples == 0)
 	{
 		n_in = in_count;
 		n_out = out_count;
+		tot_data += num2str(n_examples) + " " + num2str(n_in) + " " + num2str(n_out) + " ";
 	}
+	else if (in_count != n_in || out_count != n_out)
+	{
+		QMessageBox err_box;
+		QString err_msg = "Il numero di elementi deve essere coerente con gli esempi precedenti:":
+		if (in_count != n_in)
+			err_msg += "\nhai immesso" + num2str(in_count) + "elementi di input anziché" + num2str(n_in);
+		if (out_count != n_out)
+			err_msg += "\nhai immesso" + num2str(out_count) + "elementi di output anziché" + num2str(n_out);
+		err_box.setText(err_msg);
+		err_box.exec();
+		return;
+	}
+
 	n_examples++;
-	tot_data += std::endl + input_buffer->toPlainText() + std::endl;
-	tot_data += target_buffer->toPlainText() + std::endl;
+	tot_data += "\n" + input_buffer->toPlainText() + "\n";
+	tot_data += target_buffer->toPlainText() + "\n";
+	input_buffer->clear();
+	target_buffer->clear();
 }
 
 void dataBuilderWidget::storeData()
 {
 	addExample();
-	tot_data.insert(tot_data.begin(), num2str(n_examples) + " " + num2str(n_in) + " " + num2str(n_out) + " " );
 	QString file_title = QInputDialog::getText(this, "Save File", "Datafile title:");
 	ofstream result("./logica/data/" + file_title + ".data");
 	result << tot_data;
+	result.close();
 }
